fdda89671a4b741953fd89f5703208f278ee4f23
sml93 <shawnmlee93@gmail.com>
2022-10-22 22:59:27 +0800

    Corrected attitude control script

--- scripts/mavrosAttCtrlv2.py
+++ scripts/mavrosAttCtrlv2.py
@@ -15,9 +15,9 @@ from std_msgs.msg import Header
 from mavros_msgs.msg import ParamValue, AttitudeTarget
 from mavros_test_common import MavrosTestCommon
 from geometry_msgs.msg import Quaternion, Vector3
-# from tf.transformations import quaternion_from_euler
-from transforms3d.euler import euler2quat as quaternion_from_euler
-from transforms3d.euler import quat2euler as quaternion_to_euler
+# # from tf.transformations import quaternion_from_euler
+# from transforms3d.euler import euler2quat as quaternion_from_euler
+# from transforms3d.euler import quat2euler as quaternion_to_euler
 
 
 class MavrosOffboardAttctlTest(MavrosTestCommon):
@@ -48,7 +48,6 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
 		self.att.body_rate = Vector3()
 		self.att.header = Header()
 		self.att.header.frame_id = "base_footprint"
-		# target_quat = quaternion_from_euler(self.desired_atti.orientation.x, self.desired_atti.orientation.y, self.desired_atti.orientation.z, 'szyx')
 		self.att.orientation.w = self.desired_atti.orientation.w
 		self.att.orientation.x = self.desired_atti.orientation.x
 		self.att.orientation.y = self.desired_atti.orientation.y
@@ -60,20 +59,14 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
 		while not rospy.is_shutdown():
 			self.att.header.stamp = rospy.Time.now()
 			self.att_setpoint_pub.publish(self.att)
-			print('send_att')
 			try:
 				rate.sleep()
 			except rospy.ROSInterruptException:
 				pass
 
 	def resend_att(self):
-		# x = float(self.desired_atti.orientation.x)
-		# y = float(self.desired_atti.orientation.y)
-		# z = float(self.desired_atti.orientation.z)
 		self.att.header = Header()
 		self.att.header.frame_id = "base_footprint"
-		# thrust = float(self.desired_atti.thrust)
-		# self.att.orientation = Quaternion(*quaternion_from_euler(x, y, z))
 		self.att.orientation.w = self.desired_atti.orientation.w
 		self.att.orientation.x = self.desired_atti.orientation.x
 		self.att.orientation.y = self.desired_atti.orientation.y
@@ -85,9 +78,6 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
 									format(self.att.orientation.x, self.att.orientation.y, self.att.orientation.z, self.att.thrust))
 
 	def test_attctrl(self):
-		# boundary_x = 20
-		# boundary_y = 10
-		# boundary_z = 2
 
 		self.wait_for_topics(60)
 		self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND, 10, -1)
@@ -98,34 +88,12 @@ class MavrosOffboardAttctlTest(MavrosTestCommon):
 		self.set_mode("OFFBOARD", 5)
 		self.set_arm(True, 5)
 
-		# rospy.loginfo("attempting to cross boundary | x: {0}, y: {1}, z: {2}".format(boundary_x, boundary_y, boundary_z))
-
 		while not rospy.is_shutdown():
 			if not self.mission_done.data:
 				self.resend_att()
-				print('resend_att')
+				rospy.loginfo("resend_atti \n")
 			else:
 				break
-
-		# timeout = 90
-		# loop_freq = 2
-		# rate = rospy.Rate(loop_freq)
-		# crossed = False
-		# for i in xrange(timeout * loop_freq):
-		# 	self.resend_att()
-		# 	if (self.local_position.pose.position.x > boundary_x and
-		# 			self.local_position.pose.position.y > boundary_y and 
-		# 			self.local_position.pose.position.z > boundary_z):
-		# 		rospy.loginfo("boundary crossed | seconds: {0} of {1}".format(i/loop_freq, timeout))
-		# 		crossed = True
-		# 		break
-
-		# 	try: rate.sleep()
-		# 	except rospy.ROSException as e: self.fail(e)
-
-		# self.assertTrue(crossed, (
-		# 	"took too long to cross boundaries | current position x: {0:.2f}, y: {1:.2f}, z: {2:.2f}".
-		# 	format(self.local_position.pose.position.x, self.local_position.pose.position.y, self.local_position.pose.position.z, timeout)))
 
 		self.set_mode("AUTO.LOITER", 5)
 		t = 3
